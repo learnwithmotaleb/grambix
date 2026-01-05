e725caf80d2a7dd104d0f35767e24aa274772ecd
RakibulIslam10 <rakibulislam10.net@gmail.com>
2026-01-05 18:24:35 +0600

    sdfhb

--- lib/views/auth/login/controller/login_controller.dart
+++ lib/views/auth/login/controller/login_controller.dart
@@ -19,6 +19,9 @@ class LoginController extends GetxController {
     /// Add listeners to text controllers
     ///
     /// //PAGLA MOTALEB
+    ///
+    ///
+    /// SDAJKLFJASDLFASDFFHADJKFLJSDA;LFSDHKJLFLKSDFSDF
 
   }
 
